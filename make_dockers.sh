93d1f8a2750ee18db90a8463d338256cb921b91a
Jocelyn Giroux <jgiroux@coveo.com>
2017-08-04 10:34:28 -0400

    Update the docker build script to not tag non official tag as latest

--- make_dockers.sh
+++ make_dockers.sh
@@ -7,8 +7,14 @@ do
     printf "Processing file $df\n"
     tag=${df:12}
     tag=${tag,,}
-    version=coveo/tgf:${TRAVIS_TAG:6}${tag}
+    travis_tag=${TRAVIS_TAG:6}
+    version=coveo/tgf:${travis_tag}${tag}
     latest=${tag:1}
     latest=coveo/tgf:${latest:-latest}
-    docker build -f $df -t $version . && docker push $version && docker tag $version $latest && docker push $latest
+
+    # We do not want to tag latest if this is not an official version number
+    [[ $travis_tag == *-* ]] && unset latest
+    
+    docker build -f $df -t $version . && docker push $version &&
+    [ -n "$latest" ] && docker tag $version $latest && docker push $latest
 done
