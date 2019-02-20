575dfe76ae8386bf865d5af9e44aed338727493a
Julien Duchesne <jduchesne@coveo.com>
2019-02-20 08:33:18 -0500

    Oops add missing `must`

--- docker_unix.go
+++ docker_unix.go
@@ -11,6 +11,6 @@ import (
 const dockerSocketMountPattern = "%[1]s:%[1]s"
 
 func getDockerGroup() string {
-	s, _ := os.Stat(dockerSocketFile)
+	s := must(os.Stat(dockerSocketFile)).(os.FileInfo)
 	return fmt.Sprintf("%v", s.Sys().(*syscall.Stat_t).Gid)
 }
