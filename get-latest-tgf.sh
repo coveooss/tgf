29d107ddb445c55776585a7e012111733ee8c29a
Yann Cedric Ngadeu <ycngadeu@wks-000564.corp.coveo.com>
2019-05-08 16:00:46 -0400

    Review fixed

--- get-latest-tgf.sh
+++ get-latest-tgf.sh
@@ -5,22 +5,31 @@ get_local_tgf_version () {
 
 get_latest_tgf_version () {
     if [ -z "$GITHUB_TOKEN" ]; then
-        echo 'Warning: GITHUB_TOKEN is not set.';
+        echo 'GITHUB_TOKEN is not set.';
     fi
     TGF_LATEST_VERSION=$(curl --silent https://api.github.com/repos/coveo/tgf/releases/latest?access_token=${GITHUB_TOKEN} | grep tag_name | awk -F\" '{print $4}');
 }
 
-install_latest_tgf () {
-        curl -sL https://github.com/coveo/tgf/releases/download/v1.20.2/tgf_1.20.2_linux_64-bits.zip | gzip -d > /usr/local/bin/tgf && chmod +x /usr/local/bin/tgf;
+done_script () {
+    echo "Done.";
+    tgf --current-version;
+    exit 0;
 }
 
-if ! [ -x "$(command -v tgf)" ]; then
-  echo 'Error: tgf is not installed. Installing ...';
-  install_latest_tgf;
-  echo 'Info: Done.'
-  tgf --current-version;
-  exit 1;
-fi
+install_latest_tgf () {
+        VERSION=$(echo $TGF_LATEST_VERSION | cut -d'v' -f 2);
+        
+        if [[ "$OSTYPE" == "linux"* ]]; then
+                echo 'Installing latest tgf for Linux...';
+                curl -sL "https://github.com/coveo/tgf/releases/download/v"$VERSION"/tgf_"$VERSION"_linux_64-bits.zip" | gzip -d > /usr/local/bin/tgf && chmod +x /usr/local/bin/tgf && done_script;
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+                echo 'Installing latest tgf for OSX...';
+                curl -sL "https://github.com/coveo/tgf/releases/download/v"$VERSION"/tgf_"$VERSION"_macOS_64-bits.zip" | bsdtar -xf- -C /usr/local/bin && done_script;
+        else 
+                echo 'OS not supported.';
+                exit 1;
+        fi
+}
 
 get_local_tgf_version;
 get_latest_tgf_version;
@@ -28,11 +37,8 @@ get_latest_tgf_version;
 echo '- tgf version (local):' $TGF_LOCAL_VERSION;
 echo '- tgf version (latest):' $TGF_LATEST_VERSION;
 
-if [ "$TGF_LOCAL_VERSION" == "$TGF_LATEST_VERSION" ]
-then echo 'Info: local version is up to date';
+if [ "$TGF_LOCAL_VERSION" == "$TGF_LATEST_VERSION" ]; then 
+        echo 'Local version is up to date.';
 else
-        echo 'Warning: local version outdated, updating...';
         install_latest_tgf;
-        get_local_tgf_version;
-        echo 'Info: tgf updated to' $TGF_LOCAL_VERSION;
 fi
