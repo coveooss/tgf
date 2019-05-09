0863fe463e6bd3fc0430ae8f9957727dd6a99e2b
Yann Cedric Ngadeu <ycngadeu@wks-000564.corp.coveo.com>
2019-05-09 11:58:31 -0400

    More code styles fixes

--- get-latest-tgf.sh
+++ get-latest-tgf.sh
@@ -40,7 +40,7 @@ get_latest_tgf_version
 echo '- tgf version (local):' $TGF_LOCAL_VERSION
 echo '- tgf version (latest):' $TGF_LATEST_VERSION
 
-if [ "$TGF_LOCAL_VERSION" == "$TGF_LATEST_VERSION" ]
+if [ $TGF_LOCAL_VERSION == $TGF_LATEST_VERSION ]
 then 
         echo 'Local version is up to date.'
 else
