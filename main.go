04881fe9bffc72696d36fb431fd739937457ba3d
Yann Cedric <50204323+ycngadeu-coveo@users.noreply.github.com>
2019-05-16 11:06:59 -0400

    Getting the current version shouldn't fetch configs from AWS  (#67)
    Getting the current version shouldn't fetch configs from AWS

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"path/filepath"
 	"runtime/debug"
 
 	"github.com/coveo/gotemplate/v3/collections"
@@ -30,79 +29,7 @@ func main() {
 			os.Exit(1)
 		}
 	}()
-
-	app := NewTGFApplication(os.Args[1:])
-	config := InitConfig(app)
-
-	// If AWS profile is supplied, we freeze the current session
-	if app.AwsProfile != "" {
-		must(config.InitAWS(app.AwsProfile))
-	}
-
-	if app.Image != "" {
-		config.Image = app.Image
-		config.RecommendedImageVersion = ""
-		config.RequiredVersionRange = ""
-		config.ImageVersion = nil
-		config.ImageTag = nil
-	}
-	if app.ImageVersion != "-" {
-		config.ImageVersion = &app.ImageVersion
-	}
-	if app.ImageTag != "-" {
-		config.ImageTag = &app.ImageTag
-	}
-	if app.Entrypoint != "" {
-		config.EntryPoint = app.Entrypoint
-	}
-	if !config.ValidateVersion() {
-		os.Exit(1)
-	}
-
-	if app.GetCurrentVersion {
-		Printf("tgf v%s\n", version)
-		os.Exit(0)
-	}
-
-	if app.GetAllVersions {
-		if filepath.Base(config.EntryPoint) != "terragrunt" {
-			printError(("--all-version works only with terragrunt as the entrypoint"))
-			os.Exit(1)
-		}
-		Println("TGF version", version)
-		app.Unmanaged = []string{"get-versions"}
-	}
-
-	docker := dockerConfig{config}
-	imageName := config.GetImageName()
-	if lastRefresh(imageName) > config.Refresh || config.IsPartialVersion() || !checkImage(imageName) || app.Refresh {
-		docker.refreshImage(imageName)
-	}
-
-	if app.LoggingLevel != "" {
-		config.LogLevel = app.LoggingLevel
-	}
-
-	if app.PruneImages {
-		docker.prune(config.Image)
-		os.Exit(0)
-	}
-
-	if config.EntryPoint == "terragrunt" && app.Unmanaged == nil && !app.DebugMode && !app.GetImageName {
-		title := color.New(color.FgYellow, color.Underline).SprintFunc()
-		ErrPrintln(title("\nTGF Usage\n"))
-		app.Usage(nil)
-	}
-
-	if config.ImageVersion == nil {
-		actualVersion := docker.GetActualImageVersion()
-		config.ImageVersion = &actualVersion
-		if !config.ValidateVersion() {
-			os.Exit(2)
-		}
-	}
-
-	os.Exit(docker.call())
+	os.Exit(NewTGFApplication(os.Args[1:]).Run())
 }
 
 func printError(format string, args ...interface{})   { ErrPrintln(errorString(format, args...)) }
