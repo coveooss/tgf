a31d4c80059f62469fa5a5443884d8a171e1e3cc
Yann Cedric Ngadeu <ycngadeu@wks-000564.corp.coveo.com>
2019-06-13 12:24:17 -0400

    [DT-4] - Implemented tgf update cooldown period

--- tgf_auto_update.go
+++ tgf_auto_update.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"runtime"
+	"time"
 )
+
+var updateInterval = 2 * time.Hour
 
 // Update runs the update on the current tgf executable for unix systems
 func Update(app *TGFApplication) bool {
 	currentOs := runtime.GOOS
+
+	if lastRefresh("update") < updateInterval {
+		return false
+	}
+
+	touchImageRefresh("update")
 
 	if currentOs == "linux" || currentOs == "darwin" && RunUpdateUnix() {
 		app.Debug("tgf updated !")
